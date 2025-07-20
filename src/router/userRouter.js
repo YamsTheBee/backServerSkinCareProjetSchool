1dd6bf5c9c607a147761a1b1d0e8418a5abc2c6f
YamsTheBe <Mariamadiaw.a@hotmail.fr>
2025-07-20 17:02:33 +0200

    update src/router/userRouter.js

--- src/router/userRouter.js
+++ src/router/userRouter.js
@@ -1,39 +1,20 @@
-// const express = require("express");
+//userRouter.js
+import { Router } from "express";
+const router = Router();
 
-// const router = express.Router();
-
-// const { getOne, createOne } = require("../controller/userController");
-// // get http://localhost:4242/api/user/1
-// router.get("/:id", getOne);
-
-// // get http://localhost:4242/api/user/
-// router.post("/", createOne);
-
-// module.exports = router;
-
-// // GetOne recupérer un utilisateur
-// // POST: creation d'utilisateur
-
-const express = require("express");
-
-const {
+import {
 	register,
+	login,
+	getUser,
 	updateUser,
 	deleteUser,
-	getUser,
-} = require("../controller/userController");
+} from "../controller/userController.js";
+import authMiddleware from "../middleware/authMiddleware.js";
 
-const router = express.Router();
-
-// Route POST pour inscrire un utilisateur
 router.post("/register", register);
-// Route GET pour obtenir un utilisateur
-router.get("/:id", getUser);
+router.post("/login", login);
+router.get("/:id", authMiddleware, getUser);
+router.put("/:id", authMiddleware, updateUser);
+router.delete("/:id", authMiddleware, deleteUser);
 
-// Route PUT pour modifier les informations d'un utilisateur
-router.put("/:id", updateUser);
-
-// Route DELETE pour supprimer un utilisateur
-router.delete("/:id", deleteUser);
-
-module.exports = router;
+export default router;
