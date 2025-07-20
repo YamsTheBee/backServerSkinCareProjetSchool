180afc515ee5be71b7a49c4686534380742746fb
YamsTheBe <Mariamadiaw.a@hotmail.fr>
2025-07-20 16:54:17 +0200

    update productsRouter.js

--- src/router/productsRouter.js
+++ src/router/productsRouter.js
@@ -1,24 +1,13 @@
-const express = require("express");
-const db = require("../model/db");
-const productsController = require("../controller/productsController");
+// productsRouter.js
+import express from "express";
+import productsController from "../controller/productsController.js";
 
 const router = express.Router();
-// const { BrowserRouter } = require("react-router-dom");
 
-
-// Routes CRUD
-// Route GET pour récupérer tous les produits
 router.get("/", productsController.browse);
-
-// Route GET pour récupérer un produit par son ID
 router.get("/:id", productsController.read);
-
-// Route POST pour créer un nouveau produit
 router.post("/", productsController.create);
-
-// Route PUT pour mettre à jour un produit existant
 router.put("/:id", productsController.update);
-
-// Route DELETE pour supprimer un produit
 router.delete("/:id", productsController.remove);
-module.exports = router;
+
+export default router;
